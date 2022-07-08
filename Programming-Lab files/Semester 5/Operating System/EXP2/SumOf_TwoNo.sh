a502a94be33133b30056283f54e233797c644f60
Nitesh Kumar <nitesh20rv@gmail.com>
2022-07-08 15:33:10 +0530

    Shell program to add two numbers

---
+++ Programming-Lab files/Semester 5/Operating System/EXP2/SumOf_TwoNo.sh
@@ -0,0 +1,9 @@
+#This is a Shell script program to add two numbers
+
+echo "Enter Num1"
+read num1
+echo "Enter Num2"
+read num2
+sum=$(($num1 + $num2))
+echo "The sum of two numbers $num1 and $num2 = $sum"
+
