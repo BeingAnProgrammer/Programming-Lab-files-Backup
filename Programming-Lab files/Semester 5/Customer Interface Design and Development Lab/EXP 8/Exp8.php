eba46d3dc226fec68c8d11fc617e4888fdbd012a
Nitesh Kumar <nitesh20rv@gmail.com>
2022-09-20 14:31:55 +0530

    Working with PHP

---
+++ Programming-Lab files/Semester 5/Customer Interface Design and Development Lab/EXP 8/Exp8.php
@@ -0,0 +1,12 @@
+
+
+<html>
+    <body><br>
+        Hello! <?php echo $_POST["name"]; ?><br>
+        Your registration number is <?php echo $_POST["reg"]; ?><br>
+        You were born on <?php echo $_POST["dob"]; ?> <br>
+        You are a <?php echo $_POST["gender"]; ?> <br>
+        Your E-mail ID is <?php echo $_POST["email"]; ?><br>
+        <?php echo $_POST["mob"]; ?> is your contact number.
+    </body>
+</html>
